f7739990126be938528cdad995da6d91df64c4a3
nikita <iseliverstov59@gmail.com>
2025-12-17 14:54:33 +0700

    Добавлено решение домашнего задания Kubernetes 1.2
    - Terraform конфигурация для GKE кластера
    - Манифесты Pod и Service
    - Документация с примерами использования

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "cluster_name" {
+  description = "GKE Cluster Name"
+  value       = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  description = "GKE Cluster Endpoint"
+  value       = google_container_cluster.primary.endpoint
+  sensitive   = true
+}
+
+output "cluster_location" {
+  description = "GKE Cluster Location"
+  value       = google_container_cluster.primary.location
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${var.zone} --project ${var.project_id}"
+}
